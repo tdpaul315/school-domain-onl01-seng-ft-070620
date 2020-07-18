d2c14952a96747ec8463ef7982a7a20825f183a4
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-18 04:46:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,6 +19,15 @@ class School
     end 
   end 
 
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
+  
+end 
 
 
 school = School.new("Bayside High School")
