e235446a54230967261004c5531958f6c46cea59
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-18 04:45:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,23 @@
-def sort 
-  nu_hash = {}
-  roster.each do |x, y| 
-    nu_hash[x] = y.sort 
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
   end 
-  nu_hash
-
+  
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end 
+  
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
+  end 
 
 
 
